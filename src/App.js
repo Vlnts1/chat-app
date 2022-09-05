17708d3d2027dc01149c7141fdece9bf4d639be3
Vlnts <valentsionovaviktoria@gmail.com>
2022-09-05 16:20:40 +0300

    Create file for theme

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { ChatCol, Container, SidebarCol } from './App.styled';
+import { theme } from './styles/theme';
 import { Chat } from './views/ChatPage/Components/Chat/Chat';
 import { Sidebar } from './views/ChatPage/Components/Sidebar/Sidebar';
-import { COLORS } from './styles/colors';
-
-const theme = {
-  colors: COLORS,
-};
 
 const App = () => {
   return (
